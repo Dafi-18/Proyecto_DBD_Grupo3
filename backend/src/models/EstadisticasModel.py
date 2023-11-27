20d50ef9a52c361440d93c294d713054d0bf2afc
Dafi-18 <81888341+Dafi-18@users.noreply.github.com>
2023-11-26 20:09:45 -0500

    update estadisticas and date

--- backend/src/models/EstadisticasModel.py
+++ backend/src/models/EstadisticasModel.py
@@ -3,7 +3,8 @@ from database.dastabase import get_connection
 
 
 class EstadisticasModel:
-    @staticmethod
+
+    @classmethod
     def get_total_transacciones_mes(fecha):
         query = """
         SELECT COUNT(*) AS Total
@@ -27,5 +28,19 @@ class EstadisticasModel:
         with get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query, (fecha, fecha, fecha, fecha, fecha, fecha))
+                result = cur.fetchone()
+        return result[0] if result else None
+
+    @classmethod
+    def get_total_alquileres_mes(fecha):
+        query = """
+        SELECT COUNT(*) AS Cantidad_Alquileres
+        FROM alquiler
+        WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM %s)
+            AND EXTRACT(YEAR FROM Fecha_alquiler) = EXTRACT(YEAR FROM %s);
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha, fecha))
                 result = cur.fetchone()
         return result[0] if result else None
--- backend/src/routes/Estadisticas.py
+++ backend/src/routes/Estadisticas.py
@@ -8,8 +8,20 @@ main = Blueprint('estadisticas_blueprint', __name__)
 @main.route('/total_transacciones_mes', methods=['GET'])
 def get_total_transacciones_mes():
     try:
-        estadisticas = EstadisticasModel.get_total_transacciones_mes()
+        fecha = request.args.get('fecha')
+        estadisticas = EstadisticasModel.get_total_transacciones_mes(fecha)
         return jsonify(estadisticas), 200
+
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+
+@main.route('/total_alquileres_mes', methods=['GET'])
+def get_total_alquileres_mes():
+    try:
+        fecha = request.args.get('fecha')
+        total_alquileres = EstadisticasModel.get_total_alquileres_mes(fecha)
+        return jsonify(total_alquileres), 200
 
     except Exception as ex:
         return jsonify({'error': str(ex)}), 500
