9daad2d33e45a2b5a9a4bb69a4e34a0006a7935c
Dafi-18 <81888341+Dafi-18@users.noreply.github.com>
2023-11-27 17:32:46 -0500

    update estadisticas

--- backend/src/models/EstadisticasModel.py
+++ backend/src/models/EstadisticasModel.py
@@ -5,24 +5,24 @@ from database.dastabase import get_connection
 class EstadisticasModel:
 
     @classmethod
-    def get_total_transacciones_mes(fecha):
+    def get_total_transacciones_mes(cls, fecha):
         query = """
         SELECT COUNT(*) AS Total
         FROM (
             SELECT 'Alquiler'
             FROM Alquiler
-            WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM %s)
-              AND EXTRACT(YEAR FROM Fecha_alquiler) = EXTRACT(YEAR FROM %s)
+            WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+              AND EXTRACT(YEAR FROM Fecha_alquiler) = EXTRACT(YEAR FROM CAST(%s AS DATE))
             UNION ALL
             SELECT 'Prestamo'
             FROM Prestamo
-            WHERE EXTRACT(MONTH FROM Fecha_prestamo) = EXTRACT(MONTH FROM %s)
-              AND EXTRACT(YEAR FROM Fecha_prestamo) = EXTRACT(YEAR FROM %s)
+            WHERE EXTRACT(MONTH FROM Fecha_prestamo) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+              AND EXTRACT(YEAR FROM Fecha_prestamo) = EXTRACT(YEAR FROM CAST(%s AS DATE))
             UNION ALL
             SELECT 'Venta'
             FROM Venta
-            WHERE EXTRACT(MONTH FROM Fecha_venta) = EXTRACT(MONTH FROM %s)
-              AND EXTRACT(YEAR FROM Fecha_venta) = EXTRACT(YEAR FROM %s)
+            WHERE EXTRACT(MONTH FROM Fecha_venta) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+              AND EXTRACT(YEAR FROM Fecha_venta) = EXTRACT(YEAR FROM CAST(%s AS DATE))
         ) AS transacciones_semana;
         """
         with get_connection() as conn:
@@ -32,15 +32,88 @@ class EstadisticasModel:
         return result[0] if result else None
 
     @classmethod
-    def get_total_alquileres_mes(fecha):
+    def get_total_alquileres_mes(cls, fecha):
         query = """
         SELECT COUNT(*) AS Cantidad_Alquileres
         FROM alquiler
-        WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM %s)
-            AND EXTRACT(YEAR FROM Fecha_alquiler) = EXTRACT(YEAR FROM %s);
+        WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+            AND EXTRACT(YEAR FROM Fecha_alquiler) = EXTRACT(YEAR FROM CAST(%s AS DATE));
         """
         with get_connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(query, (fecha, fecha))
+                result = cur.fetchone()
+        return result[0] if result else None
+
+    @classmethod
+    def get_total_ventas_mes(cls, fecha):
+        query = """
+        SELECT COUNT(*) AS Cantidad_venta
+        FROM venta
+        WHERE EXTRACT(MONTH FROM Fecha_venta) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+            AND EXTRACT(YEAR FROM Fecha_venta) = EXTRACT(YEAR FROM CAST(%s AS DATE));
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha, fecha))
+                result = cur.fetchone()
+        print("Resultado:", result)  # Imprime el resultado
+        return result[0] if result else None
+
+    @classmethod
+    def get_total_prestamos_mes(cls, fecha):
+        query = """
+        SELECT COUNT(*) AS Cantidad_prestamos
+        FROM prestamo
+        WHERE EXTRACT(MONTH FROM fecha_prestamo) = EXTRACT(MONTH FROM CAST(%s AS DATE))
+            AND EXTRACT(YEAR FROM fecha_prestamo) = EXTRACT(YEAR FROM CAST(%s AS DATE));
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha, fecha))
+                result = cur.fetchone()
+        return result[0] if result else None
+
+    @classmethod
+    def get_total_recaudado_alquier_mes(cls, fecha):
+        query = """"
+        SELECT SUM(monto) AS Cantidad_Total_Monto_Recaudado_Alquiler
+        FROM alquiler
+        WHERE EXTRACT(MONTH FROM Fecha_alquiler) = EXTRACT(MONTH FROM CAST(%s AS DATE));
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha))
+                result = cur.fetchone()
+        return result[0] if result else None
+
+    @classmethod
+    def get_total_recaudado_ventas_mes(cls, fecha):
+        query = """"
+        SELECT SUM(monto_final) AS Cantidad_Total_Monto_Recaudado_Venta
+        FROM venta
+        WHERE EXTRACT(MONTH FROM fecha_venta) = EXTRACT(MONTH FROM CAST(%s AS DATE));
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha))
+                result = cur.fetchone()
+        return result[0] if result else None
+
+    @classmethod
+    def get_articulo_mas_alquilado_mes(cls, fecha):
+        query = """"
+        SELECT a.Nombre_articulo
+        FROM alquiler al
+        JOIN articulo a ON al.Id_articulo = a.Id_articulo
+        WHERE EXTRACT(MONTH FROM al.Fecha_alquiler) = EXTRACT(MONTH FROM CURRENT_DATE)
+        AND EXTRACT(YEAR FROM al.Fecha_alquiler) = EXTRACT(YEAR FROM CURRENT_DATE)
+        GROUP BY a.Nombre_articulo
+        ORDER BY COUNT(*) DESC
+        LIMIT 1;
+        """
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (fecha))
                 result = cur.fetchone()
         return result[0] if result else None
--- backend/src/routes/Estadisticas.py
+++ backend/src/routes/Estadisticas.py
@@ -8,7 +8,8 @@ main = Blueprint('estadisticas_blueprint', __name__)
 @main.route('/total_transacciones_mes', methods=['GET'])
 def get_total_transacciones_mes():
     try:
-        estadisticas = EstadisticasModel.get_total_transacciones_mes()
+        fecha = request.args.get('fecha')
+        estadisticas = EstadisticasModel.get_total_transacciones_mes(fecha)
         return jsonify(estadisticas), 200
 
     except Exception as ex:
@@ -22,5 +23,45 @@ def get_total_alquileres_mes():
         total_alquileres = EstadisticasModel.get_total_alquileres_mes(fecha)
         return jsonify(total_alquileres), 200
 
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+
+@main.route('/total_ventas_mes', methods=['GET'])
+def total_ventas_mes():
+    try:
+        fecha = request.args.get('fecha')
+        total = EstadisticasModel.get_total_ventas_mes(fecha)
+        return jsonify({'total_ventas_mes': total}), 200
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+
+@main.route('/total_prestamos_mes', methods=['GET'])
+def total_prestamos_mes():
+    try:
+        fecha = request.args.get('fecha')
+        total = EstadisticasModel.get_total_prestamos_mes(fecha)
+        return jsonify({'total_prestamos_mes': total}), 200
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+
+@main.route('/total_recaudado_alquiler_mes', methods=['GET'])
+def total_recaudado_alquiler_mes():
+    try:
+        fecha = request.args.get('fecha')
+        total = EstadisticasModel.get_total_recaudado_alquiler_mes(fecha)
+        return jsonify({'total_recaudado_alquiler_mes': total}), 200
+    except Exception as ex:
+        return jsonify({'error': str(ex)}), 500
+
+
+@main.route('/total_recaudado_venta_mes', methods=['GET'])
+def total_recaudado_ventas_mes():
+    try:
+        fecha = request.args.get('fecha')
+        total = EstadisticasModel.get_total_recaudado_ventas_mes(fecha)
+        return jsonify({'total_recaudado_venta_mes': total}), 200
     except Exception as ex:
         return jsonify({'error': str(ex)}), 500
