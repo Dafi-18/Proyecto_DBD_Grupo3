38c2edb49a370282fac3104e2e1b7cbc0fa6524e
Dafi-18 <81888341+Dafi-18@users.noreply.github.com>
2023-11-28 03:55:10 -0500

    create estadisticas front

---
+++ front/src/components/Estadistica/Estadistica.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Estadisticas extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Estadísticas</h1>
+                <h2>Hola que hace</h2>
+                {/* Aquí puedes agregar más código para mostrar tus estadísticas */}
+            </div>
+        );
+    }
+}
+
+export default Estadisticas;
