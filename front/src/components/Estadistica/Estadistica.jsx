7dba1c90566b7f0fb0652130126c40779b8b1d79
Dafi-18 <81888341+Dafi-18@users.noreply.github.com>
2023-11-28 18:40:08 -0500

    fix

--- front/src/components/Estadistica/Estadistica.jsx
+++ front/src/components/Estadistica/Estadistica.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 function Estadisticas() {
     const [totalAqlquileresMes, setTotalAlquileresMes] = useState(null);
     const [totalTransacciones, setTotalTransacciones] = useState(null);
-    const []
 
     useEffect(() => {
         const fecha = '2023-11-28'; // Formato de fecha YYYY-MM-DD
@@ -28,7 +27,7 @@ function Estadisticas() {
     return (
         <div>
             <h1>Estadísticas</h1>
-            {articuloMenosVendido && totalTransacciones ? (
+            {totalAqlquileresMes && totalTransacciones ? (
                 <div>
                     <h2>Total de alquileres al mes: {totalAqlquileresMes}</h2>
                     <h2>Total de transacciones del mes: {totalTransacciones}</h2>
